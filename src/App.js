fbe4656831b5edcac6633e2578b3556b6fbe72dd
anamul101 <rjanamul01871@gmail.com>
2022-10-10 17:49:14 +0100

    Router add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Main from './Layout/Main';
+
+
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<Main></Main>,
+      children:[
+        {
+          path:'/',
+          element:<Home></Home>
+        },
+        {
+          path:'home',
+          element:<Home></Home>
+        }
+      ]
+    }
+]);
   return (
     <div>
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
+        <nav className="navbar bg-neutral text-neutral-content">
+          
+            <div className="flex-1">
+                <Link to='/' className="btn btn-ghost normal-case text-xl">daisyUI</Link>
+            </div>
+            <div className="flex-none">
+                <NavLink to='/'>Home</NavLink>
+            </div>
             
-        </div>
+        </nav>
     );
 };
 
