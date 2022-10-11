5349f57f3e7dd0959e1388b76a738bd1d66427eb
anamul101 <rjanamul01871@gmail.com>
2022-10-11 13:55:59 +0100

    tostify add

--- src/components/Options/Options.js
+++ src/components/Options/Options.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const Options = ({qs}) => {
+const Options = ({qs,handelaAnsoewr}) => {
+    
     return (
         <div className='text-md font-bold'>
-            <h1 className='border text-left h-20 p-4 flex items-center hover:bg-sky-300'>
-                <input type="radio" name="radio-3" className="radio radio-secondary mr-2" checked />{qs}
+            <h1  className='border text-left h-20 p-4 flex items-center hover:bg-sky-300'>
+                <input onClick={()=>handelaAnsoewr()} type="radio" name="radio-3" className="radio radio-secondary mr-2" checked />{qs}
             </h1>
+            
         </div>
     );
 };
--- src/components/SingleQuiz/SingleQuiz.js
+++ src/components/SingleQuiz/SingleQuiz.js
@@ -3,14 +3,27 @@ import Options from '../Options/Options';
 
 const SingleQuiz = ({singlequiz}) => {
     const {options,correctAnswer,question,id}=singlequiz;
-    console.log(singlequiz);
+    const handelaAnsoewr=()=>{
+        let message;
+        if(correctAnswer){
+            message=alert(correctAnswer);
+        }
+        else if(!correctAnswer){
+            message=alert('worng');
+        }
+        
+        return message;
+    }
+    // console.log(singlequiz);
     return (
         <div className='bg-green-500 my-8 p-8 w-[70%] mx-auto'>
             <h1 className='text-center my-8 text-2xl font-bold'>quiz:{question}</h1>
             <div className=' grid grid-cols-2 gap-4 w-[90%] mx-auto p-4'>
+                
                 {
-                    options.map(qs=><Options qs={qs}></Options>)
+                    options.map(qs=><Options qs={qs} handelaAnsoewr={handelaAnsoewr}></Options>)
                 }
+                
             </div>
         </div>
     );
